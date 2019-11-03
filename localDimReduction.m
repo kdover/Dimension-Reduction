0deb17d4e9939463516dba85087247e053117c97
kdover <kdover@users.noreply.github.com>
2019-11-02 17:29:11 -0700

    Update localDimReduction.m
    Found another type, also now has a plot feature

--- localDimReduction.m
+++ localDimReduction.m
@@ -1,4 +1,4 @@
-function [ Y ] = localDimReduction(data, Trials, eta, K)
+function [ Y ] = localDimReduction(data,target, Trials, eta, K,plot)
     dataSize = length(data(:,1));
     [Idx, D] = knnsearch(data,data,'K',K);
     %calc local dim, store n and t in 2 row vector
@@ -13,9 +13,10 @@ function [ Y ] = localDimReduction(data, Trials, eta, K)
     globalN = 2*(mu^2)/(sigma^2);
     
     %initialize our data as normal, small variance
-    Y = 1.001*randn(dataSize,2);
+    Y = 0.001*randn(dataSize,2);
     trialNum = 0;
     while trialNum < Trials
+        %disp(trialNum);
         for i = 1:dataSize
             localN = localDim(1,i);
             localT = localDim(2,i);
@@ -27,19 +28,24 @@ function [ Y ] = localDimReduction(data, Trials, eta, K)
                     highDist = localT*origDist(i,j);
                     %if dim < 3, just compare dist directly
                     if localN < 3
-                        if actual_y_dist < highDist
+                        %if too big, push apart
+                        if actual_y_dist > highDist
                             Y(i,:)=Y(i,:)-eta*dif;
                         else
+                            %if too small, pull together
                             Y(i,:) = Y(i,:)+eta*dif;
                         end
                     else
                         xbound = localN-2;
                         expectedDist = -2*log(1-chi2cdf(highDist,localN));
                         ybound = -2*log(1-chi2cdf(xbound,localN));
+                        %if y is much larger than x, pull together
                         if actual_y_dist > expectedDist && highDist < xbound
                             Y(i,:)=Y(i,:)-eta*dif;
-                         elseif actual_y_dist < ybound && highDist > xbound
+                         %if y is much smaller than x, push apart
+                        elseif actual_y_dist < ybound && highDist > xbound
                              Y(i,:)=Y(i,:)+eta*actual_y_dist*dif;
+                        %if y is just smaller than x, push apart
                         elseif actual_y_dist < expectedDist && highDist < xbound
                             Y(i,:) = Y(i,:)+eta*dif;
                         end
@@ -49,8 +55,10 @@ function [ Y ] = localDimReduction(data, Trials, eta, K)
                     actual_y_dist = norm(dif)^2;
                     highDist = globalT*origDist(i,j);
                     if globalN < 3
+                        %if too large, pull together
                         if actual_y_dist > highDist
                             Y(i,:)= Y(i,:)-eta*dif;
+                        %else, push apart
                         else
                             Y(i,:)= Y(i,:)+eta*dif;
                         end
@@ -58,16 +66,23 @@ function [ Y ] = localDimReduction(data, Trials, eta, K)
                         expectedDist = -2*log(1-chi2cdf(highDist,globalN));
                         xbound = globalN-2;
                         ybound = -2*log(1-chi2cdf(xbound,globalN));
+                        %if y is much larger than x, pull together
                         if actual_y_dist > expectedDist && highDist < xbound
                             Y(i,:) = Y(i,:)-eta*dif;
-                         elseif actual_y_dist < ybound && highDist > xbound
+                        %if y is much smaller than x, push apart
+                        elseif actual_y_dist < ybound && highDist > xbound
                              Y(i,:) = Y(i,:)+eta*actual_y_dist*dif;
+                        %if y is just smaller than x, push apart
                         elseif actual_y_dist < expectedDist && highDist < xbound
                             Y(i,:) = Y(i,:)+eta*dif;
                         end
                     end
                 end
             end
+        end
+        if plot == 1
+           gscatter(Y(:,1),Y(:,2),target);
+           drawnow
         end
         trialNum = trialNum+1;
     end
