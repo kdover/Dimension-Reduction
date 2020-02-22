d967338fffa3265174ca40e8cae59e2a18ba5823
kdover <kdover@users.noreply.github.com>
2020-02-21 17:15:59 -0800

    Update
    Has piecewise gradient

--- grad_function.m
+++ grad_function.m
@@ -1,7 +1,13 @@
 function [ c ] = grad_function(low_dim_dist,expected_dist)
     dif = low_dim_dist-expected_dist;
-    %c = 2*dif;
-    %c =2*(dif)^(-3)*exp(-(dif)^(-2));
-    c = sign(dif)*sqrt(abs(dif))*exp(-(dif)^(-2));
-    %c = sign(dif)*exp(-(dif)^(-2));
+    %c = 2*sign(dif)*(dif).^2;
+    if dif > 0
+        c =2*(dif)^(-1)*exp(-0.2*(dif)^(-2));
+    else
+        c = sign(dif)*exp(-0.2*(dif)^(-2));
+    end
+    %c = 2*(dif).^(-3).*exp(-(dif).^(-2));
+    %c = sign(dif)*sqrt(abs(dif))*exp(-(dif)^(-2));
+    %c = sign(dif)*exp(-0.2*(dif)^(-2));
+    %c = dif;
 end
